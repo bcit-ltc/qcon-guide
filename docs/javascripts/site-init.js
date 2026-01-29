cc8ce18fe00169d212e6663eed8a529a8ebc8e6c
Arvin Rolos <arolos@bcit.ca>
2026-01-29 10:43:45 -0800

    fix: add plausible script

---
+++ docs/javascripts/site-init.js
@@ -0,0 +1,7 @@
+window.plausible = window.plausible || function () {
+  (window.plausible.q = window.plausible.q || []).push(arguments);
+};
+window.plausible.o = {
+  captureOnLocalhost: false,
+  autoCapturePageviews: true,
+};
