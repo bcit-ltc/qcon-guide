2ba8ed421ece8f54eaf8611898645285f1d6bf06
Arvin Rolos <arolos@bcit.ca>
2026-02-20 14:19:55 -0800

    fix: separate domain for plausible

--- docs/javascripts/site-init.js
+++ docs/javascripts/site-init.js
@@ -1,7 +1,3 @@
-window.plausible = window.plausible || function () {
-  (window.plausible.q = window.plausible.q || []).push(arguments);
-};
-window.plausible.o = {
-  captureOnLocalhost: false,
-  autoCapturePageviews: true,
-};
+window.plausible = window.plausible || function () { (window.plausible.q = window.plausible.q || []).push(arguments); };
+window.plausible.o = { captureOnLocalhost: false, autoCapturePageviews: true };
+window.ltcDomain = { eventDomain: "qcon-guide.ltc.bcit.ca" };
